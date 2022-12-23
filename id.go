0319a42dcc6d221d8f3198bdbb82fb8480b39390
ugurkorkmaz <korkmaz.uu@gmail.com>
2022-12-23 23:02:00 +0300

    feat/create: unique identifier of the entity

---
+++ id.go
@@ -0,0 +1,29 @@
+package mixin
+
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/contrib/entproto"
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// UUID is a mixin that adds a UUID field to an entity.
+type Id struct {
+	ent.Mixin
+}
+
+// Fields of the UUID mixin.
+func (Id) Fields() []ent.Field {
+	return []ent.Field{
+		field.
+			UUID("id", uuid.UUID{}).
+			Annotations(
+				entgql.OrderField("ID"),
+				entproto.Field(1),
+			).
+			Default(uuid.New).
+			Immutable().
+			Comment("The unique identifier of the entity."),
+	}
+}
