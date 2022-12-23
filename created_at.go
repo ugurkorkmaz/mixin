00f833596bdb5825cf97ecdfa617e7de65963e7a
ugurkorkmaz <korkmaz.uu@gmail.com>
2022-12-23 23:02:29 +0300

    feat/create: created_at field

---
+++ created_at.go
@@ -0,0 +1,30 @@
+package mixin
+
+import (
+	"time"
+
+	"entgo.io/contrib/entgql"
+	"entgo.io/contrib/entproto"
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
+
+// CreatedAt mixin for the created_at field.
+type CreatedAt struct {
+	ent.Mixin
+}
+
+// Fields of the CreatedAt mixin.
+func (CreatedAt) Fields() []ent.Field {
+	return []ent.Field{
+		field.
+			Time("created_at").
+			Immutable().
+			Annotations(
+				entgql.OrderField("CREATED_AT"),
+				entproto.Field(2),
+			).
+			Default(time.Now).
+			Comment("The time when the entity was created."),
+	}
+}
